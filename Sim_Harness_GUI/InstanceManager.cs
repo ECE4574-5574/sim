db4854ec620538ecdc0c73ee78f42cdf0af325d1
bt2016 <bt2016@vt.edu>
2015-04-20 14:14:44 -0400

    Install the App from Sim Harness
    For the App Generator location on the GUI, the user should point to a
    folder that has at least the following files: adb.sh,
    com.homeAutomationApp.apk, and location_of_adb_dir.txt.
    
    The user must also have Android SDK installed and the Nexus 5 emulator
    must be running already

--- Sim_Harness_GUI/InstanceManager.cs
+++ Sim_Harness_GUI/InstanceManager.cs
@@ -22,6 +22,7 @@ public class InstanceManager{
 
 	//NOTE: names are from the parent's (this program's) perspective
 
+	//this function gets called when the 'startTestButton' button is clicked
 	public bool startGeneratorProcesses(string appLocation, string houseLocation, string timeFrameBlob, string testScenarioBlob){
 
 		_timeFrameInfo = timeFrameBlob;
@@ -41,7 +42,7 @@ public class InstanceManager{
 		}
 
 		//TODO: set up how to start the mobile app
-
+		startOneApp(appLocation);
 
 
 
@@ -65,6 +66,80 @@ public class InstanceManager{
 		
 
 	//  helper functions //
+
+	private string startOneApp(string dir){
+		string output = "";
+
+		//load resource files
+		string location_of_terminal = @"C:\Windows\System32\cmd.exe";
+		string batchFile = dir + "\\spawn_app.bat";
+		string shellScript = dir + "\\adb.sh";
+		string appInstaller = dir + "\\com.homeAutomationApp.apk";
+		StreamReader sr = new StreamReader(dir + "\\location_of_adb_dir.txt");
+		string adbExe = sr.ReadLine() + "\\adb";
+		sr.Close();
+
+		//check for non-existent files
+		if(!File.Exists(batchFile)){
+			output += "Could not find 'spawn_app.bat' in " + dir + "\n";
+			output += "Can not spawn app.";
+			return output;
+		}
+		if(!File.Exists(shellScript)){
+			output += "Could not find 'adb.sh' in " + dir + "\n";
+			output += "Can not spawn app.";
+			return output;
+		}
+		if(!File.Exists(appInstaller)){
+			output += "Could not find 'com.homeAutomationApp.apk' in " + dir + "\n";
+			output += "Can not spawn app.";
+			return output;
+		}
+		if(!File.Exists(adbExe + ".exe")){
+			output += "Could not find 'adb.exe' in " + adbExe + "\n";
+			output += "Can not spawn app.";
+			return output;
+		}
+
+		//this process will send commands to adb.exe, and install the app
+		//set information about the process
+		ProcessStartInfo p_info = new ProcessStartInfo();
+		StreamWriter child_stdin;
+		StreamReader child_stdout;
+		StreamReader child_stderr;
+		p_info.FileName = location_of_terminal;
+		p_info.UseShellExecute = false;
+		p_info.ErrorDialog = false;
+		p_info.RedirectStandardError = true;
+		p_info.RedirectStandardInput = true;
+		p_info.RedirectStandardOutput = true;
+
+		//start the process
+		Process p = new Process();
+		p.StartInfo = p_info;
+		Console.WriteLine(startProcess(ref p, ref p_info));
+		child_stdin = p.StandardInput;
+		child_stdout = p.StandardOutput;
+		child_stderr = p.StandardError;
+
+		//send the batch file to the process
+		string command_1 = adbExe + " shell pm uninstall com.homeAutomationApp";
+		string command_2 = adbExe + " push " + appInstaller + " /data/local/";
+		string command_3 = adbExe + " push " + shellScript + " /data/local/";
+		string command_4 = adbExe + " shell pm install /data/local/com.HomeAutomationApp.apk";
+		child_stdin.WriteLine(command_1);
+		child_stdout.ReadLine();
+		child_stdin.WriteLine(command_2);
+		child_stdout.ReadLine();
+		child_stdin.WriteLine(command_3);
+		child_stdout.ReadLine();
+		child_stdin.WriteLine(command_4);
+		while(child_stdout.ReadLine() != "Success"){}
+
+
+		p.Kill();
+		return output;
+	}
 
 	/**
 	 * Attempts to start a process with particualar information. If the process starts
