d2d9c6b4f7969674613850bf98a5db12daa4212f
bt2016 <bt2016@vt.edu>
2015-05-01 15:56:10 -0400

    updated launch.sh

--- Sim_Harness_GUI/launch.sh
+++ Sim_Harness_GUI/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb shell pm uninstall com.homeAutomationApp
-~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb push com.homeAutomationApp.apk /data/local/
-~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb push adb.sh /data/local/
+~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb push $1 /data/local/
+~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb push $2 /data/local/
 ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb shell pm install /data/local/com.homeAutomationApp.apk
 ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb shell am start -n com.homeAutomationApp/homeautomationapp.droid.MainActivity
 ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb shell am start -n com.homeAutomationApp/homeautomationapp.droid.MainActivity
