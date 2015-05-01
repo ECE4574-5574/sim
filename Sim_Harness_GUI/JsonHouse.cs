85460bcb46669117eb719aeecbe69add4d7f636e
Clayton Kuchta <clayton.kuchta@gmail.com>
2015-05-01 12:28:35 -0400

    Add initial files

---
+++ Sim_Harness_GUI/JsonHouse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Sim_Harness_GUI
+{
+public class JsonHouse
+{
+	string name{ get; }
+	string id{ get; }
+
+
+	public JsonHouse(JToken house)
+	{
+		
+	}
+}//class
+}//namespace
+
---
+++ Sim_Harness_GUI/JsonUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sim_Harness_GUI
+{
+public class JsonUser
+{
+	string name{get;}
+	int id {get;}
+
+	public JsonUser()
+	{
+	}
+
+
+}//class
+}//namespace
+
