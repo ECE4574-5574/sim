a38434166447bf7d3d78545bdb26f9769634deb8
Jason Ziglar <jpz@vt.edu>
2015-05-02 01:37:45 -0400

    Adding time specified temperature access.

--- sim_weather/Weather.cs
+++ sim_weather/Weather.cs
@@ -8,7 +8,19 @@ namespace Hats.SimWeather
  */
 public interface IWeather
 {
+	/**
+	 * Gets the temperature at the current time, according to the given TimeFrame.
+	 * \param[out] Current temperature in Celsius, NaN if no setpoints exist
+	 */
 	Double Temperature();
+	/**
+	 * Gets the temperature at the given time. Will convert the given time to the
+	 * internal TimeFrame if requested, otherwise assumes the given time is valid in that TimeFrame.
+	 * \param[in] now Time to get temperature for.
+	 * \param[in] convertTime Flag indicating if time should be converted.
+	 * \param[out] Temperature in Celsius
+	 */
+	Double Temperature(DateTime now, bool convert_time = false);
 };
 
 }
